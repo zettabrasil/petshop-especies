86046641a6bacba7f8943b6804c5277688acf023
Darlei Kroth <maildarlei@icloud.com>
2020-01-24 09:24:36 -0300

    feat: adicionadas especies e racas inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+const canino = require('./lib/canino');
+const coelho = require('./lib/coelho');
+const felino = require('./lib/felino');
+const outros = require('./lib/outros');
+const passaro = require('./lib/passaro');
+const peixe = require('./lib/peixe');
+const reptil = require('./lib/reptil');
+const roedor = require('./lib/roedor');
+
+module.exports = {
+  'Canino': canino,
+  'Coelho': coelho,
+  'Felino': felino,
+  'Outros': outros,
+  'Pássaro': passaro,
+  'Peixe': peixe,
+  'Réptil': reptil,
+  'Roedor': roedor,
+};
